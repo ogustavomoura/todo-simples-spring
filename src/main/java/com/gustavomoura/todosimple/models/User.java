abae4a606517bfe98bfb1561300a3ba206a2ee28
Gustavo <2101971@aluno.univesp.br>
2024-08-06 05:37:52 -0300

    Finalizada Aula 05 - Criando o model de User (Java Web)

---
+++ src/main/java/com/gustavomoura/todosimple/models/User.java
@@ -0,0 +1,104 @@
+package com.gustavomoura.todosimple.models;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+	
+	public interface CreateUser { }
+	
+	public interface UpdateUser { }
+	
+	public static final String TABLE_NAME = "user";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true)
+	private Long id;
+	
+	@Column(name = "username", length = 100, nullable = false, unique = true)
+	@NotNull(groups = CreateUser.class)
+	@NotEmpty(groups = CreateUser.class)
+	@Size(groups = CreateUser.class, min = 2, max = 100)
+	private String username;
+	
+	@JsonProperty(access = Access.WRITE_ONLY)
+	@Column(name = "password", length = 60, nullable = false)
+	@NotNull(groups = {CreateUser.class, UpdateUser.class})
+	@NotNull(groups = {CreateUser.class, UpdateUser.class})
+	@Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+	private String password;
+	
+//	private List<Task> tasks = new ArrayList<Task>();
+
+	public User() {
+		
+	}
+
+	public User(Long id, String username, String password) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, password, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id) && Objects.equals(password, other.password)
+				&& Objects.equals(username, other.username);
+	}
+	
+
+	
+	
+
+}
