1afed92fa520cda30e871b269133f4b1816eeb93
Gustavo <2101971@aluno.univesp.br>
2024-08-07 06:06:53 -0300

    Finalizada aula 12 - Criando o controller para Task (Java Web)

--- src/main/java/com/gustavomoura/todosimple/controllers/TaskController.java
+++ src/main/java/com/gustavomoura/todosimple/controllers/TaskController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gustavomoura.todosimple.models.Task;
 import com.gustavomoura.todosimple.services.TaskService;
+import com.gustavomoura.todosimple.services.UserService;
 
 import jakarta.validation.Valid;
 
@@ -29,6 +30,9 @@ public class TaskController {
 	@Autowired
 	private TaskService taskService;
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<Task> findById(@PathVariable Long id){
 		Task obj = this.taskService.findById(id);
@@ -37,6 +41,7 @@ public class TaskController {
 	
 	@GetMapping("/user/{userId}")
 	public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId){
+		userService.findById(userId);
 		List<Task> objs = this.taskService.findAllByUserId(userId);
 		return ResponseEntity.ok().body(objs);
 	}
