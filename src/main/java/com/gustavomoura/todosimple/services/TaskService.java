60293cf56b14d0ed8e7a7790029b13a4ef76bf1f
Gustavo <2101971@aluno.univesp.br>
2024-08-06 21:23:19 -0300

    Finalizada aula 12 - Criando o controller para Task (Java Web)

--- src/main/java/com/gustavomoura/todosimple/services/TaskService.java
+++ src/main/java/com/gustavomoura/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.gustavomoura.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,11 @@ public class TaskService {
 					"Não é possível excluir pois há entidades relacionadas"
 					);
 		}
+	}
+	
+	public List<Task> findAllByUserId(Long userId){
+		List<Task> tasks = this.taskRepository.buscarTarefasPorIdDeUsuario(userId);
+		return tasks;
 	}
 
 }
